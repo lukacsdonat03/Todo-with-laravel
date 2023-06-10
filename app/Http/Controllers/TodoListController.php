a4dd82ae4e3678beedfb9a08b963342681821e35
lukacsdonat03 <lukacsdonatxxx@gmail.com>
2023-06-10 11:32:20 +0200

    auth done and delete button

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -21,20 +21,40 @@ class TodoListController extends Controller
         return redirect('/');
     }
 
-    public function markInCompleted($id){
-        $listItem = ListItem::find($id);
-        $listItem->is_completed = 0;
-        $listItem->save();
+    public function markInCompleted($id, Request $request){
         
+        if($request->input('action') === 'aprove' ){
+            $listItem = ListItem::find($id);
+            $listItem->is_completed = 0;
+            $listItem->save();
+        }else if($request->input('action') === 'delete' ){
+                   $data = ListItem::find($id);
+                   if($data){
+                    $data->delete();
+                    session()->flash('success','Deleted successfully.');
+                   }else{
+                    session()->flash('error','Data not found');
+                   } 
+        }
         return redirect('/');
     }
 
-    public function saveItem(Request $request){
+    public function saveItem($id,Request $request){
         $newListItem = new ListItem;
         $newListItem->name = $request->listItem;
         $newListItem->is_completed = 0;
         $newListItem->save();
+        if($request->input('action') === 'deny' ){
 
+        }else if($request->input('action') === 'delte' ){
+           $data = ListItem::find($id);
+           if($data){
+            $data->delete();
+            session()->flash('success','Deleted successfully.');
+           }else{
+            session()->flash('error','Data not found');
+           }             
+        }
         return redirect('/');
     }
 }
